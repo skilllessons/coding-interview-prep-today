ed1cf7cfa533494fe8cc3c06d81265b62a9cee9c
soumya_acharya <cse.soumya@gmail.com>
2020-08-08 16:22:09 -0700

    Commit

--- src/main/java/array/ThreeSum.java
+++ src/main/java/array/ThreeSum.java
@@ -7,17 +7,27 @@ import java.util.List;
 public class ThreeSum {
 
     public List<List<Integer>> threeSum(int[] nums) {
-
-        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-
-
-
-        for(int i =0;i<nums.length; i++){
-           // checkCombo(i)
-
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length && nums[i] <= 0; ++i)
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSumII(nums, i, res);
+            }
+        return res;
+    }
+    void twoSumII(int[] nums, int i, List<List<Integer>> res) {
+        int lo = i + 1, hi = nums.length - 1;
+        while (lo < hi) {
+            int sum = nums[i] + nums[lo] + nums[hi];
+            if (sum < 0) {
+                ++lo;
+            } else if (sum > 0) {
+                --hi;
+            } else {
+                res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
+                while (lo < hi && nums[lo] == nums[lo - 1])
+                    ++lo;
+            }
         }
-
-       return res;
     }
 }
