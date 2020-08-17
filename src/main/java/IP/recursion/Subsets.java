47fc1825c5dc672d8d68c663f3c03ab8ccf0ee93
soumya_acharya <cse.soumya@gmail.com>
2020-08-17 01:32:01 -0700

    Commit

--- src/main/java/IP/recursion/Subsets.java
+++ src/main/java/IP/recursion/Subsets.java
@@ -3,7 +3,31 @@ package main.java.IP.recursion;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CombinationSet {
+/**
+ *
+ * https://leetcode.com/problems/subsets/
+ *
+ * Given a set of distinct integers, nums, return all possible subsets (the power set).
+ *
+ * Note: The solution set must not contain duplicate subsets.
+ *
+ * Example:
+ *
+ * Input: nums = [1,2,3]
+ * Output:
+ * [
+ *   [3],
+ *   [1],
+ *   [2],
+ *   [1,2,3],
+ *   [1,3],
+ *   [2,3],
+ *   [1,2],
+ *   []
+ * ]
+ */
+
+public class Subsets {
 
 
     public static List<List<Integer>> CombinationSet(int[] nums){
--- src/main/java/IP/recursion/letterCaseCombinations.java
+++ src/main/java/IP/recursion/letterCaseCombinations.java
@@ -6,31 +6,31 @@ import java.util.List;
 
 /**
  * https://leetcode.com/problems/letter-case-permutation/
- *
+ * <p>
  * Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.
- *
+ * <p>
  * Return a list of all possible strings we could create. You can return the output  in any order.
-
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: S = "a1b2"
  * Output: ["a1b2","a1B2","A1b2","A1B2"]
  * Example 2:
- *
+ * <p>
  * Input: S = "3z4"
  * Output: ["3z4","3Z4"]
  * Example 3:
- *
+ * <p>
  * Input: S = "12345"
  * Output: ["12345"]
  * Example 4:
- *
+ * <p>
  * Input: S = "0"
  * Output: ["0"]
- *
- *
+ * <p>
+ * <p>
  * Constraints:
- *
+ * <p>
  * S will be a string with length between 1 and 12.
  * S will consist only of letters or digits.
  */
@@ -41,33 +41,57 @@ public class letterCaseCombinations {
     public static List<String> letterCasePermutation(String input) {
         List<String> result = new ArrayList<>();
         String slate = "";
-        int index=0;
+        int index = 0;
 
 
-        letterCasePermutationHelper(input, slate,index,result);
+        letterCasePermutationHelper(input, slate, index, result);
 
         return result;
     }
 
     private static void letterCasePermutationHelper(String input, String slate, int index, List<String> result) {
 
-        if(index == input.length()) {
+        if (index == input.length()) {
             result.add(slate);
             return;
         }
 
-        if(Character.isDigit(input.charAt(index))){
-            letterCasePermutationHelper(input,slate+input.charAt(index),index+1,result);
+        if (Character.isDigit(input.charAt(index))) {
+            letterCasePermutationHelper(input, slate + input.charAt(index), index + 1, result);
 
         } else {
-            letterCasePermutationHelper(input,slate+Character.toLowerCase(input.charAt(index)),index+1,result);
-            letterCasePermutationHelper(input,slate+Character.toUpperCase(input.charAt(index)),index+1,result);
+            letterCasePermutationHelper(input, slate + Character.toLowerCase(input.charAt(index)), index + 1, result);
+            letterCasePermutationHelper(input, slate + Character.toUpperCase(input.charAt(index)), index + 1, result);
 
         }
     }
 
 
+    private static void letterCasePermutationHelper1(String input, StringBuilder slate, int index, List<String> result) {
 
+        if (index == input.length()) {
+            result.add(slate.toString());
+            return;
+        }
+
+        if (Character.isDigit(input.charAt(index))) {
+            slate.append(input.charAt(index));
+            letterCasePermutationHelper(input, slate.toString(), index + 1, result);
+            slate.deleteCharAt(input.charAt(index));
+
+
+        } else {
+
+            slate.append(input.charAt(index));
+            letterCasePermutationHelper(input, slate.toString(), index + 1, result);
+            slate.deleteCharAt(input.charAt(index));
+
+            slate.append(input.charAt(index));
+            letterCasePermutationHelper(input, slate.toString(), index + 1, result);
+            slate.deleteCharAt(input.charAt(index));
+
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -76,8 +100,8 @@ public class letterCaseCombinations {
 
         List<String> resultInput = new ArrayList<>();
 
-       // letterCasePermutationHelper1("a1b2", new StringBuilder(),0,resultInput);
-       // System.out.println(result);
+        letterCasePermutationHelper1("a1b2", new StringBuilder(), 0, resultInput);
+        System.out.println(result);
 
     }
 
