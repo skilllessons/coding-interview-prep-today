aa6002ee43e7fffd779fe77e8b7f915c4027e2a0
soumya_acharya <cse.soumya@gmail.com>
2020-09-07 23:37:20 -0700

    Commit

---
+++ src/main/java/companies/README.md
@@ -0,0 +1,5 @@
+# Amazon
+
+
+    -<b>Amazon</b>.</br>
+      - <a href="https://leetcode.com/problems/two-sum/">1. Two Sum</a>.</br>
---
+++ src/main/java/companies/amazon/TwoSum.java
@@ -0,0 +1,41 @@
+package main.java.companies.amazon;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class TwoSum {
+    public static int[] sumOfTwo(int[] input, int sum) {
+
+        Arrays.sort(input);
+        int low = 0;
+        int high = input.length - 1;
+        int[] result = new int[2];
+
+        while (low < high) {
+            if (input[low] + input[high] == sum) {
+                result[0] = input[low];
+                result[1] = input[high];
+                return result;
+            } else if (input[low] + input[high] > 0) {
+                high--;
+
+            } else if (input[low] + input[high] > 0) {
+                low++;
+
+            }
+
+        }
+        return result;
+    }
+
+
+    @Test
+    public void testSumOfTwo(){
+        int[] input = {2, 7, 11, 15};
+        int[] result = {2, 7};
+        assertEquals(result,sumOfTwo(input, 9));
+    }
+}
