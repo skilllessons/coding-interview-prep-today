8b1df22e39d99f34a94aee1da3f2b4156b54390b
soumya_acharya <cse.soumya@gmail.com>
2020-09-07 23:50:18 -0700

    Commit

---
+++ src/main/java/companies/amazon/NumIslands.java
@@ -0,0 +1,76 @@
+package main.java.companies.amazon;
+
+/**
+ * 200. Number of Islands
+ * Medium
+ *
+ * 6262
+ *
+ * 206
+ *
+ * Add to List
+ *
+ * Share
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: grid = [
+ *   ["1","1","1","1","0"],
+ *   ["1","1","0","1","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","0","0","0"]
+ * ]
+ * Output: 1
+ * Example 2:
+ *
+ * Input: grid = [
+ *   ["1","1","0","0","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","1","0","0"],
+ *   ["0","0","0","1","1"]
+ * ]
+ * Output: 3
+ */
+public class NumIslands {
+
+    public int numIslands(char[][] grid) {
+
+        if(grid == null || grid.length == 0){
+            return 0;
+        }
+
+        int numIslands = 0;
+
+        for(int row=0; row<grid.length; row++){
+            for(int col=0;col<grid[0].length;col++){
+                if(grid[row][col] == '1'){
+                    numIslands++;
+                    dfs(grid,row,col);
+                }
+            }
+
+
+        }
+
+        return numIslands;
+    }
+
+
+    void dfs(char[][] grid, int row, int col){
+
+        if( row < 0 ||  col<0 || row >= grid.length || col>= grid[0].length ||
+                grid[row][col] == '0' ) {
+            return;
+        }
+
+        grid[row][col] = '0';
+        dfs(grid,row-1,col);
+        dfs(grid,row+1,col);
+        dfs(grid,row,col-1);
+        dfs(grid,row,col+1);
+
+    }
+}
--- src/main/java/companies/amazon/TwoSum.java
+++ src/main/java/companies/amazon/TwoSum.java
@@ -2,40 +2,27 @@ package main.java.companies.amazon;
 
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
 
 public class TwoSum {
-    public static int[] sumOfTwo(int[] input, int sum) {
-
-        Arrays.sort(input);
-        int low = 0;
-        int high = input.length - 1;
-        int[] result = new int[2];
-
-        while (low < high) {
-            if (input[low] + input[high] == sum) {
-                result[0] = input[low];
-                result[1] = input[high];
-                return result;
-            } else if (input[low] + input[high] > 0) {
-                high--;
-
-            } else if (input[low] + input[high] > 0) {
-                low++;
-
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                return new int[]{map.get(nums[i]),i};
+            }else{
+                map.put(target - nums[i], i);
             }
-
         }
-        return result;
+        return null;
     }
-
 
     @Test
     public void testSumOfTwo(){
         int[] input = {2, 7, 11, 15};
         int[] result = {2, 7};
-        assertEquals(result,sumOfTwo(input, 9));
+        assertEquals(result,twoSum(input, 9));
     }
 }
