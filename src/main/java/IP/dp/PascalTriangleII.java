776134f62a3ccb8dd8060fadda88bebaf3cc9d82
soumya_acharya <cse.soumya@gmail.com>
2020-09-10 00:23:35 -0700

    Commit

--- src/main/java/IP/dp/PascalTriangleII.java
+++ src/main/java/IP/dp/PascalTriangleII.java
@@ -50,36 +50,25 @@ import java.util.List;
 public class PascalTriangleII {
 
     public List<Integer> getRow(int rowIndex) {
-
-
         List<List<Integer>> triangle = new ArrayList<>();
-
         if (rowIndex == 0) {
             List<Integer> input = new ArrayList<>();
             input.add(1);
             return input;
-
-
         }
-
 
         List<Integer> first_row = new ArrayList<>();
         first_row.add(1);
-
         triangle.add(first_row);
-
         for (int i = 1; i < rowIndex + 1; i++) {
             List<Integer> prev_row = triangle.get(i - 1);
             List<Integer> row = new ArrayList<>();
             row.add(1);
-
             for (int j = 1; j < i; j++) {
                 row.add(prev_row.get(j - 1) + prev_row.get(j));
             }
             row.add(1);
-
             triangle.add(row);
-
         }
 
 
