58ae7fee1a49ac5d0bc94ec3e58ad15d690953b2
soumya_acharya <cse.soumya@gmail.com>
2020-09-26 17:00:28 -0700

    Commit

---
+++ src/main/java/proxy/Greeter.java
@@ -0,0 +1,38 @@
+package main.java.proxy;
+
+abstract class Greeter {
+    abstract String greet();
+}
+
+
+class RealGreater extends Greeter {
+
+
+    @Override
+    String greet() {
+        return "Hi there!";
+    }
+}
+
+
+class GreeterProxy extends Greeter {
+
+    Greeter greeter;
+
+    @Override
+    String greet() {
+
+        if(greeter == null) greeter = new RealGreater();
+        return greeter.greet();
+    }
+}
+
+class Test{
+    public static void main(String[] args) {
+        Greeter greeter = new GreeterProxy();
+        System.out.println(greeter.greet());
+        System.out.println(greeter.greet());
+
+    }
+
+}
