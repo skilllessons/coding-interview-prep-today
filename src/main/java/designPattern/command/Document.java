4b4eb445687e3328ba34e5b2ff5d1ef5d4c0a842
soumya_acharya <cse.soumya@gmail.com>
2020-09-26 17:22:27 -0700

    Commit

---
+++ src/main/java/designPattern/command/Document.java
@@ -0,0 +1,59 @@
+package main.java.designPattern.command;
+
+public class Document {
+
+    String text = "";
+}
+
+abstract class DocumentCommand {
+    Document document;
+
+    public DocumentCommand(Document document) {
+        this.document = document;
+    }
+
+    abstract void execute();
+}
+
+class AddCharCommand extends DocumentCommand {
+    char c;
+
+    public AddCharCommand(Document document, char c) {
+        super(document);
+        this.c = c;
+    }
+
+    @Override
+    void execute() {
+        document.text += c;
+    }
+}
+
+class DeleteCommand extends DocumentCommand {
+
+
+    public DeleteCommand(Document document) {
+        super(document);
+    }
+
+    @Override
+    void execute() {
+        document.text = document.text.substring(0, document.text.length() - 1);
+    }
+}
+
+
+class test {
+    public static void main(String[] args) {
+        Document d = new Document();
+        DocumentCommand[] dc = {new AddCharCommand(d, 'A'), new AddCharCommand(d, 'p'), new AddCharCommand(d, 'p'),
+                new AddCharCommand(d, 'l'), new AddCharCommand(d, 'e'), new DeleteCommand(d)};
+
+        for (DocumentCommand c : dc) {
+            c.execute();
+        }
+        System.out.println(d.text);
+
+    }
+}
+
