07fb60a0a5a332409e43298d69f7c1bb5be67b71
soumya_acharya <cse.soumya@gmail.com>
2020-11-01 13:32:38 -0800

    Commit

--- src/main/java/IP/arraySorting/MeetingRoomII.java
+++ src/main/java/IP/arraySorting/MeetingRoomII.java
@@ -37,29 +37,19 @@ public class MeetingRoomII {
     public int minMeetingRooms(int[][] intervals) {
         if(intervals == null || intervals.length == 0) return 0;
 
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                return a[0] -b[0];
-            }
-        });
+        Arrays.sort(intervals,(int[] a, int[] b) -> (a[0] - b[0]));
 
-        //Arrays.sort(intervals,(int[] a, int[] b) -> (a[0] - b[0]));
-        PriorityQueue<Integer> minheap = new PriorityQueue<>(( a,b) -> a - b);
+        PriorityQueue<Integer> minheap = new PriorityQueue<>((a,b) -> a - b);
+
         minheap.add(intervals[0][1]);
 
-        for(int i=0;i<intervals.length;i++)   {
-
+        for(int i=1;i<intervals.length;i++)   {
             if(intervals[i][0] >= minheap.peek()){
                 minheap.poll();
-
             }
             minheap.add(intervals[i][1]);
-
         }
-
         return minheap.size();
-
     }
 
 
@@ -68,7 +58,7 @@ public class MeetingRoomII {
         int[][] intervals = new int[3][3];
 
         int[] i1 = new int[2];
-        i1[0] = 15;
+        i1[0] = 0;
         i1[1] = 30;
 
 
