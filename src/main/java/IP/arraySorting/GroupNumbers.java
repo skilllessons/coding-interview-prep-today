c1a387263122097fd6e52bec39d5ade6b931e5a8
soumya_acharya <cse.soumya@gmail.com>
2020-11-02 00:10:56 -0800

    Commit

--- src/main/java/IP/arraySorting/GroupNumbers.java
+++ src/main/java/IP/arraySorting/GroupNumbers.java
@@ -29,8 +29,10 @@ public class GroupNumbers {
     }
 
     public static void main(String[] args) {
-        int[] arr = {3,4,1};
-        solve(arr);
+        int[] arr = {3,4,1,2,3,5,6,7};
+        int[] results = solve(arr);
+
+        System.out.println(results.toString());
 
     }
 
