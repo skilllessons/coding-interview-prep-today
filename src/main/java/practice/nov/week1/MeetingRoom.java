b16cf0cb93ecbe4bab00f86f87dfec3a86869835
soumya_acharya <cse.soumya@gmail.com>
2020-11-11 00:59:18 -0800

    Commit

--- src/main/java/practice/nov/week1/MeetingRoom.java
+++ src/main/java/practice/nov/week1/MeetingRoom.java
@@ -3,6 +3,28 @@ package main.java.practice.nov.week1;
 import java.util.Arrays;
 import java.util.Comparator;
 
+/**
+ * Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: intervals = [[0,30],[5,10],[15,20]]
+ * Output: false
+ * Example 2:
+ *
+ * Input: intervals = [[7,10],[2,4]]
+ * Output: true
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= intervals.length <= 104
+ * intervals[i].length == 2
+ * 0 <= starti < endi <= 106
+ */
+
 public class MeetingRoom {
 
     public boolean canAttendMeetings(int[][] intervals) {
