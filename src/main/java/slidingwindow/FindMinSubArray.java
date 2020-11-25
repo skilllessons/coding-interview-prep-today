f3aac7b3317ae0581fab6dfe3cff8444867951fb
soumya_acharya <cse.soumya@gmail.com>
2020-11-24 21:21:05 -0800

    Commit

--- src/main/java/slidingwindow/FindMinSubArray.java
+++ src/main/java/slidingwindow/FindMinSubArray.java
@@ -11,7 +11,7 @@ public class FindMinSubArray {
         for (int i=0; i<arr.length; i++) {
             windowSum += arr[i];
 
-            if(windowSum>= S){
+            while(windowSum>= S){
                 minLength = Math.min(minLength, i - windowstart+1);
                 windowSum -= arr[windowstart];
                 windowstart++;
@@ -31,9 +31,9 @@ public class FindMinSubArray {
 
         int result = FindMinSubArray.findMinSubArray(7, new int[] { 2, 1, 5, 2, 3, 2 });
         System.out.println("Smallest subarray length: " + result);
-        //result = FindMinSubArray.findMinSubArray(7, new int[] { 2, 1, 5, 2, 8 });
-        //System.out.println("Smallest subarray length: " + result);
-        //result = FindMinSubArray.findMinSubArray(8, new int[] { 3, 4, 1, 1, 6 });
-        //System.out.println("Smallest subarray length: " + result);
+        result = FindMinSubArray.findMinSubArray(7, new int[] { 2, 1, 5, 2, 8 });
+        System.out.println("Smallest subarray length: " + result);
+        result = FindMinSubArray.findMinSubArray(8, new int[] { 3, 4, 1, 1, 6 });
+        System.out.println("Smallest subarray length: " + result);
     }
 }
