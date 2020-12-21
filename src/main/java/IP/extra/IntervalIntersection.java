cdac692eb4a3eb865f18786a6d39a79b1c08e352
soumya_acharya <cse.soumya@gmail.com>
2020-12-21 00:00:48 -0800

    Commit

--- src/main/java/IP/extra/IntervalIntersection.java
+++ src/main/java/IP/extra/IntervalIntersection.java
@@ -10,9 +10,6 @@ public class IntervalIntersection {
         int i = 0, j = 0;
 
         while (i < A.length && j < B.length) {
-            // Let's check if A[i] intersects B[j].
-            // lo - the startpoint of the intersection
-            // hi - the endpoint of the intersection
             int lo = Math.max(A[i][0], B[j][0]);
             int hi = Math.min(A[i][1], B[j][1]);
             if (lo <= hi)
